3f08ce319e58846a8b84222592a91d11bbd2526f
brody sullivan <brodysullivan0@gmail.com>
2020-06-16 01:52:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,27 @@
 class Dog
-  
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts "#{dog}"
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end
